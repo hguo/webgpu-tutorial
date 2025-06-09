4fa502f123843e251fce42ea2569e8efccad3d44
Hanqi Guo <hanqiguo@Qingyas-MBP.ctc>
2025-06-09 16:21:28 +0800

    offset colored triangle

---
+++ 05-offset-colored-triangle.js
@@ -0,0 +1,120 @@
+async function main()
+{
+  // get webgpu adapter and device
+  const adaptor = await navigator.gpu?.requestAdapter();
+  const device = await adaptor?.requestDevice();
+  if (!device) {
+    fail('your browser does not support WebGPU');
+    return;
+  }
+
+  // create a webgpu context with the canvas
+  const canvas = document.getElementById("webgpu-canvas");
+  const context = canvas.getContext("webgpu");
+  const format = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({device, format});
+
+  // vertex and fragment shaders (in one single module)
+  const module = device.createShaderModule({
+    label: 'vertex buffer triangle',
+    code: `
+      struct VSOut {
+        @builtin(position) pos : vec4f,
+        @location(0) color : vec4f,
+      };
+
+      @vertex fn vs(
+        @location(0) pos : vec2f, 
+        @location(1) color : vec4f
+      ) -> VSOut
+      {
+        var out : VSOut;
+        out.pos = vec4f(pos, 0.0, 1.0);
+        out.color = color;
+        return out;
+      }
+
+      @fragment fn fs(vsOut : VSOut) -> @location(0) vec4f 
+      {
+        return vsOut.color;
+      }
+    `,
+  });
+
+  // the rendering pipeline
+  const pipeline = device.createRenderPipeline({
+    label: 'vertex buffer triangle pipeline',
+    layout: 'auto',
+    vertex: {
+      entryPoint: 'vs',
+      module: module, 
+      buffers: [
+        { 
+          arrayStride: 5 * 4, // 5 floating-point numbers with 4 bytes each
+          attributes: [
+            {
+              shaderLocation: 0,
+              offset: 0,
+              format: 'float32x2',
+            },
+            {
+              shaderLocation: 1,
+              offset: 2 * 4, // the offset is two floating-point numbers
+              format: 'float32x2',
+            }
+          ]
+        },
+      ],
+    },
+    fragment: {
+      entryPoint: 'fs',
+      module: module,
+      targets: [{ format: format }],
+    },
+  });
+
+  // vertex position and color data in one buffer
+  const vertexData = new Float32Array([
+    0.0, 0.0, 1.0, 0.0, 0.0, 
+    0.5, 0.0, 0.0, 1.0, 0.0,
+    0.5, 0.5, 0.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 0.0, 0.0, 
+    0.5, 0.5, 0.0, 0.0, 1.0,
+    0.0, 0.5, 1.0, 1.0, 0.0
+  ]);
+
+  // vertex buffer for both positions and colors
+  const vertexBuffer = device.createBuffer({
+    label: 'vertex position buffer of both positions and colors for two triangles',
+    size: vertexData.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
+  device.queue.writeBuffer(vertexBuffer, 0, vertexData);
+
+  const render = () => {
+    const textureView = context.getCurrentTexture().createView(); // the output is a texture, and we are getting a "view" of texture as the output of the render pass
+    const renderPassDescriptor = {
+      colorAttachments: [{
+        view: textureView,
+        clearValue: [1.0, 1.0, 1.0, 1.0], // an arbitrary color you prefer
+        storeOp: 'store',
+        loadOp: 'clear',
+      }],
+    };
+
+    const commandEncoder = device.createCommandEncoder();
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);
+    passEncoder.setVertexBuffer(0, vertexBuffer);
+    passEncoder.setVertexBuffer(1, vertexBuffer);
+    passEncoder.draw(vertexData.length / 5); 
+    passEncoder.end();
+
+    // fire up the GPU to render the load value to the output texture
+    device.queue.submit([commandEncoder.finish()]);
+  };
+
+  render();
+}
+
+main();
